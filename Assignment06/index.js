df2ca1f31dfbea6b367fc48e6dbb6ec3f5cb9649
umairkhanofficial <umairkhansadiq@gmail.com>
2023-09-14 07:52:36 -0700

    13 Ecercise added

--- Assignment06/index.js
+++ Assignment06/index.js
@@ -38,6 +38,33 @@ console.log("-".repeat(20));
 console.log(5 + 3);
 console.log("-".repeat(20));
 console.log();
-console.log(`----------------------------\n`);
-console.log(`console.log(5+3)\n`);
-console.log(`----------------------------\n`);
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let FavNum = 7;
+console.log(`My favorite Number is ${FavNum} `);
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//Muhammad Umair Date 9/11/2023 adding the favorite num in variable then print with message!
+let FavNum1 = 7;
+console.log(`My favorite Number is ${FavNum1} `);
+console.log();
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+{
+    let names = ["UK", "Ben", "KHAN"];
+    for (let index = 0; index < names.length; index++) {
+        const element = names[index];
+        console.log(element);
+    }
+}
+//console.log(names[0]);
+//console.log(names[1]);
+//console.log(names[2]);
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names = ["UK", "Ben", "Harry"];
+for (let index = 0; index < names.length; index++) {
+    const element = names[index];
+    console.log("Hello! Where are you", element);
+}
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+let transVehicle = ["Hounda", "Bmx", "Ferrari"];
+transVehicle.forEach((vehicle) => {
+    console.log(`i would love to ride the ${vehicle}`);
+});
