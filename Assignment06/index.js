ec06f58fbbe9a974f6e7a80121e9e826f8d31ab2
umairkhanofficial <umairkhansadiq@gmail.com>
2023-09-15 11:01:15 -0700

    15 Exercise done

--- Assignment06/index.js
+++ Assignment06/index.js
@@ -70,7 +70,18 @@ transVehicle.forEach((vehicle) => {
     console.log(`i would love to ride the ${vehicle}`);
 });
 //Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
-let arr = ["Harry", "Adam", "Brooke"];
-for (let index = 0; index < arr.length; index++) {
-    console.log(`Lets gather around the table MR ${arr[index]} and share a meal together`);
+let guests = ["Harry", "Adam", "Brooke"];
+for (let index = 0; index < guests.length; index++) {
+    console.log(`Lets gather around the table MR ${guests[index]} and share a meal together`);
+}
+console.log();
+//Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+//• Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+console.log(`${guests[2]} Guest who cant Join the party`);
+//• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+console.log("add new Friend in list to invite");
+guests.splice(2, 1, "Zamba");
+//• Print a second set of invitation messages, one for each person who is still in your list.
+for (let index = 0; index < guests.length; index++) {
+    console.log(`Alert new Message to all Lets gather around the table Mr ${guests[index]} and share a meal together`);
 }
